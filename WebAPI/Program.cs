413770d88c0ffd5c2d2ceeee70772b2ef9f44c86
Dilek <dilekyal1@gmail.com>
2023-07-22 20:59:36 +0300

    added JWT

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,10 +1,16 @@
 using Autofac;
+using Autofac.Core;
 using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
 using Business.DependencyResolvers.Autofac;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +27,7 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
 {
     builder.RegisterModule(new AutofacBusinessModule());
 });
-var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
@@ -30,8 +36,27 @@ var app = builder.Build();
 //    app.UseSwaggerUI();
 //}
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenOptions:SecurityKey"])),
+    };
+});
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
 
+var app = builder.Build();
+app.UseRouting();
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
